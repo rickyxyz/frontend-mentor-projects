365a5487871ce048590f970df54c55e8fe156b07
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2022-06-13 12:45:38 +0900

    added JS and styled both card state

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -1,8 +1,17 @@
 const ratingForm = document.getElementsByClassName("rating-form")[0];
+const cardBefore = document.getElementById("content_before");
+const cardAfter = document.getElementById("content_after");
+const ratingResult = document.getElementById("rating_result");
 
 ratingForm.addEventListener("submit", (eventSubmit) => {
     eventSubmit.preventDefault();
 
     const formData = new FormData(ratingForm);
     const rating = formData.get("rating");
+
+    // console.log(`🎉 ${rating}`);
+    cardBefore.classList.toggle("card_hidden");
+    cardAfter.classList.toggle("card_hidden");
+
+    ratingResult.innerHTML = `You selected ${rating} out of 5`;
 });
