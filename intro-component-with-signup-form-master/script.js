4c948ed616f22f655e9d8c9a8395526727bd5adf
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2022-07-11 14:56:21 +0900

    improve element scaling

---
+++ intro-component-with-signup-form-master/script.js
@@ -0,0 +1,6 @@
+const signupForm = document.getElementById("signup-form");
+const firstNameInput = document.getElementById("first-name");
+const lastNameInput = document.getElementById("last-name");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const submitButton = document.getElementById("submit-button");
