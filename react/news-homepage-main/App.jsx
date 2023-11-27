64bf7d9f5a17149965e025399db1c313a11a80e8
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2023-11-28 05:10:11 +0700

    feat: add static mobile styling

--- react/news-homepage-main/App.jsx
+++ react/news-homepage-main/App.jsx
@@ -8,7 +8,7 @@ function App() {
           <h1>
             <img src="./assets/images/logo.svg" alt="Logo" />
           </h1>
-          <nav>
+          <nav className="navigation-maximized">
             <ul className="header__navigation">
               <li>
                 <a href="">Home</a>
@@ -27,60 +27,63 @@ function App() {
               </li>
             </ul>
           </nav>
+          <button className="navigation-minimized">
+            <img src="./assets/images/icon-menu.svg" alt="" />
+          </button>
         </header>
-        <section className="hero">
-          <article>
-            <img src="./assets/images/image-web-3-desktop.jpg" alt="" />
-            <h2>The Bright Future of Web 3.0?</h2>
-            <p>
-              We dive into the next evolution of the web that claims to put the
-              power of the platforms back into the hands of the people. But is
-              it really fulfilling its promise?
-            </p>
-            <a href="">Read more</a>
-          </article>
-        </section>
-        <section className="aside">
-          <article>
-            <span>01</span>
-            <h1>Reviving Retro PCs</h1> What happens when old PCs are given
-            modern upgrades?
-          </article>
-          <article>
-            <span>02</span>
-            <h1>Top 10 Laptops of 2022</h1> Our best picks for various needs and
-            budgets.
-          </article>
-          <article>
-            <span>03</span>
-            <h1>The Growth of Gaming</h1> How the pandemic has sparked fresh
-            opportunities.
-          </article>
-        </section>
-        <aside className="feed">
-          <img src="./assets/images/image-retro-pcs.jpg" alt="" />
-          <h1>New</h1>
-          <section>
-            <h2>Hydrogen VS Electric Cars</h2>
-            <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
-          </section>
-          <section>
-            <img src="./assets/images/image-top-laptops.jpg" alt="" />
-            <h2>The Downsides of AI Artistry</h2>
-            <p>
-              What are the possible adverse effects of on-demand AI image
-              generation?
-            </p>
-          </section>
-          <section>
-            <img src="./assets/images/image-gaming-growth.jpg" alt="" />
-            <h2>Is VC Funding Drying Up?</h2>
-            <p>
-              Private funding by VC firms is down 50% YOY. We take a look at
-              what that means.
-            </p>
-          </section>
+        <article className="hero">
+          <img src="./assets/images/image-web-3-mobile.jpg" alt="" />
+          <h2>The Bright Future of Web 3.0?</h2>
+          <p>
+            We dive into the next evolution of the web that claims to put the
+            power of the platforms back into the hands of the people. But is it
+            really fulfilling its promise?
+          </p>
+          <a href="">Read more</a>
+        </article>
+        <aside className="aside">
+          <h2>New</h2>
+          <div className="aside__list">
+            <section>
+              <h3>Hydrogen VS Electric Cars</h3>
+              <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
+            </section>
+            <section>
+              <h3>The Downsides of AI Artistry</h3>
+              <p>
+                What are the possible adverse effects of on-demand AI image
+                generation?
+              </p>
+            </section>
+            <section>
+              <h3>Is VC Funding Drying Up?</h3>
+              <p>
+                Private funding by VC firms is down 50% YOY. We take a look at
+                what that means.
+              </p>
+            </section>
+          </div>
         </aside>
+        <section className="feed">
+          <article className="feed__article">
+            <img src="./assets/images/image-retro-pcs.jpg" alt="" />
+            <span>01</span>
+            <h2>Reviving Retro PCs</h2>
+            <p>What happens when old PCs are given modern upgrades?</p>
+          </article>
+          <article className="feed__article">
+            <img src="./assets/images/image-top-laptops.jpg" alt="" />
+            <span>02</span>
+            <h2>Top 10 Laptops of 2022</h2>
+            <p>Our best picks for various needs and budgets.</p>
+          </article>
+          <article className="feed__article">
+            <img src="./assets/images/image-gaming-growth.jpg" alt="" />
+            <span>03</span>
+            <h2>The Growth of Gaming</h2>
+            <p>How the pandemic has sparked fresh opportunities.</p>
+          </article>
+        </section>
       </main>
       {/* <footer className="attribution">
         Challenge by
