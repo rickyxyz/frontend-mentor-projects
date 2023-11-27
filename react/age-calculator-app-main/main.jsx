c982df8ce462092c36d7ac9e21a7ef8e1be1e914
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2023-11-27 16:43:15 +0700

    fix: set attribution to footer, make h1 hidden

--- react/age-calculator-app-main/main.jsx
+++ react/age-calculator-app-main/main.jsx
@@ -5,12 +5,12 @@ import App from "./App";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <div className="attribution">
+    <footer className="attribution">
       Challenge by{" "}
       <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
         Frontend Mentor
       </a>
       . Coded by <a href="https://github.com/rickyxyz">rickyxyz</a>.
-    </div>
+    </footer>
   </React.StrictMode>
 );
