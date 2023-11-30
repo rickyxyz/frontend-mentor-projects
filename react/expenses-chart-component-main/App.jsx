cb468ba65e045b3b1a125aeca9b58277511749f8
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2023-12-01 01:10:24 +0700

    setup code

--- react/expenses-chart-component-main/App.jsx
+++ react/expenses-chart-component-main/App.jsx
@@ -1,4 +1,32 @@
+const DATA = {
+  balance: 921.48,
+  monthly: 478.33,
+  changes: 2.4,
+  week: [48.35, 71.63, 18.22, 5.05, 78.94, 56.03, 80.11],
+};
+
+function Chart({ weeklyData }) {
+  const heightMap = weeklyData;
+
+  return (
+    <div className="chart">
+      {weeklyData.map((data) => {
+        return <div>{data}</div>;
+      })}
+    </div>
+  );
+}
+
 function App() {
+  // My balance $921.48 Spending - Last 7 days mon tue wed thu fri sat sun Total
+  // this month $478.33 +2.4% from last month
+
+  // <div class="attribution">
+  //   Challenge by
+  //   <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+  //     >Frontend Mentor</a
+  //   >. Coded by <a href="#">Your Name Here</a>.
+  // </div>
   return <></>;
 }
 
--- react/expenses-chart-component-main/vite.config.js
+++ react/expenses-chart-component-main/vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://rickyxyz.github.io/frontendmentor-projects/notifications-page-main",
+  base: "https://rickyxyz.github.io/frontendmentor-projects/expenses-chart-component-main",
   build: {
-    outDir: "../../notifications-page-main",
+    outDir: "../../expenses-chart-component-main",
   },
 });
