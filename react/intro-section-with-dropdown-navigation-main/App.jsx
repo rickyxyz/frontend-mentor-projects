3b3d0f08d9be85c84e2465a13a3b4a1e7aea110d
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2023-12-01 16:41:58 +0700

    setup: intro section with dropdown navigation

---
+++ react/intro-section-with-dropdown-navigation-main/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      Features Company Careers About Login Register Make remote work Get your
+      team in sync, no matter your location. Streamline processes, create team
+      rituals, and watch productivity soar. Learn more
+      <div class="attribution">
+        Challenge by
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/intro-section-with-dropdown-navigation-main/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "https://rickyxyz.github.io/frontendmentor-projects/intro-section-with-dropdown-navigation-main",
+  build: {
+    outDir: "../../intro-section-with-dropdown-navigation-main",
+  },
+});
