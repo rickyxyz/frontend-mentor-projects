aa9f8d499d0c41de7fc45714c58b18a218035b17
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2023-12-01 16:24:55 +0700

    feat: add expense-chart-component

--- react/expenses-chart-component-main/App.jsx
+++ react/expenses-chart-component-main/App.jsx
@@ -1,13 +1,4 @@
-/** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
-
-const styles = {
-  main: css({
-    margin: 10,
-    padding: 10,
-    backgroundColor: "#eee",
-  }),
-};
+import logo from "./images/logo.svg";
 
 const DATA = {
   balance: 921.48,
@@ -15,30 +6,67 @@ const DATA = {
   changes: 2.4,
   week: [48.35, 71.63, 18.22, 5.05, 78.94, 56.03, 80.11],
 };
+const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 
 function Chart({ weeklyData }) {
-  const heightMap = weeklyData;
+  const _max = Math.max(...weeklyData);
+  const heightMap = weeklyData.map((data) => data / _max);
+  const today = Math.max(0, new Date().getDay() - 1);
 
   return (
     <div className="chart">
-      {weeklyData.map((data) => {
-        return <div>{data}</div>;
+      {heightMap.map((data, i) => {
+        return (
+          <div key={`bar-${i}-${data}`} className="chart__bar_wrapper">
+            <div className="chart__bar_label">${weeklyData[i]}</div>
+            <div
+              className={`chart__bar ${i == today ? "chart__bar-today" : ""}`}
+              style={{ height: `calc(calc(100% - 2.5rem) * ${data})` }}
+            />
+            <span className="chart__bar_legend">{weekdays[i]}</span>
+          </div>
+        );
       })}
     </div>
   );
 }
 
 function App() {
-  // My balance $921.48 Spending - Last 7 days mon tue wed thu fri sat sun Total
-  // this month $478.33 +2.4% from last month
-
-  // <div class="attribution">
-  //   Challenge by
-  //   <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
-  //     >Frontend Mentor</a
-  //   >. Coded by <a href="#">Your Name Here</a>.
-  // </div>
-  return <></>;
+  return (
+    <>
+      <header className="header">
+        <div className="header__text">
+          <h2>My balance</h2>
+          <span>$921.48</span>
+        </div>
+        <img src={logo} alt="logo" />
+      </header>
+      <main>
+        <section className="section-graph">
+          <h1 className="main__header">Spending - Last 7 days</h1>
+          <Chart weeklyData={DATA.week} />
+        </section>
+        <hr />
+        <section className="section-2_columns main__total">
+          <h2>Total this month</h2>
+          <span className="main__total__total">
+            ${DATA.week.reduce((total, val) => total + val, 0).toFixed(2)}
+          </span>
+          <div className="main__total__changes">
+            <span>+2.4%</span>
+            <p>from last month</p>
+          </div>
+        </section>
+      </main>
+      <footer className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="https://github.com/rickyxyz">@rickyxyz</a>.
+      </footer>
+    </>
+  );
 }
 
 export default App;
