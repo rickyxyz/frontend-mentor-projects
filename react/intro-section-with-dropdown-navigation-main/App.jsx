1d6ff92169e14395a9b52a58d13b6a4e3364bb30
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2023-12-06 00:35:24 +0700

    feat: add intro scetion with dropdown navigation

--- react/intro-section-with-dropdown-navigation-main/App.jsx
+++ react/intro-section-with-dropdown-navigation-main/App.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 import "./style.css";
+import databiz from "./images/client-databiz.svg";
+import audiophile from "./images/client-audiophile.svg";
+import meet from "./images/client-meet.svg";
+import maker from "./images/client-maker.svg";
 
 function App() {
   return (
-    <div>
-      Features Company Careers About Login Register Make remote work Get your
-      team in sync, no matter your location. Streamline processes, create team
-      rituals, and watch productivity soar. Learn more
-      <div className="attribution">
-        Challenge by
-        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-          Frontend Mentor
-        </a>
-        . Coded by <a href="#">Your Name Here</a>.
+    <main className="main">
+      <div className="main__banner" />
+      <div className="main__content">
+        <div className="main__text">
+          <h1>Make remote work</h1>
+          <p>
+            Get your team in sync, no matter your location. Streamline
+            processes, create team rituals, and watch productivity soar.
+          </p>
+        </div>
+        <button className="button-filled">Learn more</button>
+        <div className="main__testimonials">
+          <img src={databiz} alt="databiz" />
+          <img
+            src={audiophile}
+            alt="audiophile"
+            className="main__testimonials-audiophile"
+          />
+          <img src={meet} alt="meet" />
+          <img src={maker} alt="maker" />
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
 
--- react/intro-section-with-dropdown-navigation-main/main.jsx
+++ react/intro-section-with-dropdown-navigation-main/main.jsx
@@ -8,5 +8,12 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Header />
     <App />
+    {/* <footer className="attribution">
+      Challenge by
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        Frontend Mentor
+      </a>
+      . Coded by <a href="#">Your Name Here</a>.
+    </footer> */}
   </React.StrictMode>
 );
